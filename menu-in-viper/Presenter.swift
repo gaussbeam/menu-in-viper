169308c9fc2d59340f9c038f66f031deb819b27b
gaussbeam <koji.hanyu@studist.jp>
2023-04-11 10:40:08 +0900

    Add Presenter, Wireframe

---
+++ menu-in-viper/Presenter.swift
@@ -0,0 +1,12 @@
+//
+//  Presenter.swift
+//  menu-in-viper
+//
+//  Created by HANYU, Koji on 2023/04/11.
+//
+
+protocol Presentation {
+}
+
+final class Presenter: Presentation {
+}
---
+++ menu-in-viper/Wireframe.swift
@@ -0,0 +1,12 @@
+//
+//  Wireframe.swift
+//  menu-in-viper
+//
+//  Created by HANYU, Koji on 2023/04/11.
+//
+
+protocol Wireframe {
+}
+
+struct Router: Wireframe {
+}
